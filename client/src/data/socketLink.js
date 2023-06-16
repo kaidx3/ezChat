22c5bbb09f425d63df8cf7baa01d4d2d61ee29d1
Kaiden Flaig <Kaiden.Flaig@bunch.ca>
2023-06-16 10:36:28 -0600

    fixed api link

--- client/src/data/socketLink.js
+++ client/src/data/socketLink.js
@@ -1,4 +1,4 @@
-const socketLink = "http://localhost:5000";
-//const socketLink = "http://143.110.209.44:5000";
+//const socketLink = "http://localhost:5000";
+const socketLink = "http://143.110.209.44:5000";
 
 export {socketLink}
